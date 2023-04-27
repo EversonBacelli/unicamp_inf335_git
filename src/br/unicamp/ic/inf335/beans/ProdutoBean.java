e0e9be8f225d288e60931869d1fcf7f45f480e8e
Bacelli <everson.willian29@gmail.com>
2023-04-27 10:02:00 -0300

    Atualizada a classe ProdutoBean no método CompareTo, pois o operador relacional estava invertido

--- src/br/unicamp/ic/inf335/beans/ProdutoBean.java
+++ src/br/unicamp/ic/inf335/beans/ProdutoBean.java
@@ -66,7 +66,7 @@ public class ProdutoBean implements java.io.Serializable, Comparable<ProdutoBean
 	
 	@Override
 	public int compareTo(ProdutoBean p) {
-		if (valor > p.getValor()) {
+		if (valor < p.getValor()) {
 			return 1;
 		} else if (valor > p.getValor()) {
 			return -1;
