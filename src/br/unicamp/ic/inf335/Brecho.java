57f05a6e1eb4a2e30df8efa8a1af9fd42b10f17d
Bacelli <everson.willian29@gmail.com>
2023-04-27 09:55:01 -0300

    Realizado as correções nos laços de repetição: iniciado na posição 0 e configurado limite menor que o tamanho dos laços

--- src/br/unicamp/ic/inf335/Brecho.java
+++ src/br/unicamp/ic/inf335/Brecho.java
@@ -18,7 +18,7 @@ public class Brecho {
 		produtos.add(new ProdutoBean("CD00006","Prod 6 ...", "Bla Bla Bla",1500.0,"Bla Bla Bla"));
 		
 		// Imprime produtos
-		for (int i=1; i<=produtos.size(); i++) {
+		for (int i=0; i<produtos.size(); i++) {
 			System.out.println("Codigo = " + produtos.get(i).getCodigo() + " Nome = " + produtos.get(i).getNome() + " Valor = " + produtos.get(i).getValor());
 		}
 		
@@ -27,14 +27,14 @@ public class Brecho {
 		
 		System.out.println("-------------------- Produtos Ordenados -------------------");
 		// Imprime produtos ordenados
-		for (int i=1; i<=produtos.size(); i++) {
+		for (int i=0; i<produtos.size(); i++) {
 			System.out.println("Codigo = " + produtos.get(i).getCodigo() + " Nome = " + produtos.get(i).getNome() + " Valor = " + produtos.get(i).getValor());
 		}
 		
 		// Calcula Média
 		Double media = 0.0;
-		int i = 1;
-		while (i<=produtos.size()) {
+		int i = 0;
+		while (i<produtos.size()) {
 			media = produtos.get(i).getValor();
 			i++;
 		}
